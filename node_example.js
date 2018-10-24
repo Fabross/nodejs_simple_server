cc6d0557405d01e81ee0371dd8a20e0ea9e45c7f
Fabricio <fabricio.rosales.12@sansano.usm.cl>
2018-10-23 23:51:00 -0300

    Added multiplication and fibonacci

--- node_example.js
+++ node_example.js
@@ -23,6 +23,56 @@ var server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'application/json'})
     response.end(JSON.stringify({result:suma}))
   }
+
+  if(data[0] === '/multiplicacion') {
+    var multiplicacion = 1
+    var input = data[1].split('&')
+    input.forEach(function(numero) {
+      var splitted = numero.split('=')[1]
+      if(isNumeric(splitted)) {
+        //Ok, it's a number
+        var number = Number(splitted)
+        multiplicacion *= number
+      } else {
+        //It's not a number, so we show an error message and sends to the client
+        response.writeHead(400, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+      }
+    })
+    //If everithing was OK, we send code 200 response, using json format
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify({result:multiplicacion}))
+  }
+
+  if(data[0] === '/fibonacci') 
+  {
+    var fibonacci = ['1']
+    var input = data[1].split('=')[1]
+    var n = input.split('&')[0]
+    if(isNumeric(n))
+    {
+      for (i=1; i<Number(n);i++)
+      {
+        if(fibonacci.length===1)
+        {
+          fibonacci.push('1')
+        }
+        else
+        {
+          fibonacci.push(((Number(fibonacci[i-1])+Number(fibonacci[i-2]))).toString())
+        }
+      }
+    }
+    else 
+    {
+      //It's not a number, so we show an error message and sends to the client
+      response.writeHead(400, {'Content-Type': 'application/json'})
+      response.end(JSON.stringify({result:'error, el input debe ser numerico',n}))
+    }
+    //If everithing was OK, we send code 200 response, using json format
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify({result:fibonacci}))
+  }
 })
 
 /**
